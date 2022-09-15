792ce33cd672be8e4059858c5b7a44ad49226387
sudip120 <110006135+sudip120@users.noreply.github.com>
2022-09-15 08:30:30 +0545

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var typing=new Typed(".text", {
+    strings: ["", "Youtuber", "Freelancer", "Graphics Designer", "Web Designer", "Web Developer"],
+    typeSpeed: 200,
+    backSpeed: 80,
+    loop: true,
+});
